3e03027719bbc009c8f16895deb84866b4f4e882
al8876 <al8876@gmail.com>
2018-01-24 03:45:13 +0000

    Added post function for URL

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 var urlDatabase = {
@@ -22,11 +25,21 @@ app.get('/hello', (req, res) => {
   res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new")
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
 app.get('/urls/:id', (req, res) => {
   let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
   console.log(templateVars);
   res.render('urls_show', templateVars);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
