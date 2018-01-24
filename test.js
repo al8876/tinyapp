56cb755cb5cc156eaf2ed4911a77d3130a5e934c
al8876 <al8876@gmail.com>
2018-01-24 02:08:07 +0000

    added shortened URL list to table format in HTML

---
+++ test.js
@@ -0,0 +1,9 @@
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+for (key in urlDatabase) {
+  console.log("This is the key: " + key);
+  console.log("This is the value:" + urlDatabase[key]);
+}
